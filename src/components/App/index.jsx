eb7840de8e1af2bcb501f2b255af68a90e4f3b1a
Arthur Barros <135854588+th3gust@users.noreply.github.com>
2024-02-06 21:09:59 -0300

    add new operators

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -3,6 +3,10 @@ import Button from "../Button";
 import { Container, Wrapper, Row } from "./styles";
 import { arrOperators } from "../../utils/Operators";
 import * as math from "mathjs";
+import { FaDivide, FaMinus, FaPlus, FaSquareRootAlt, FaTimes } from "react-icons/fa";
+import { TiBackspaceOutline } from "react-icons/ti";
+
+
 
 const App = () =>{
     
@@ -28,6 +32,34 @@ const App = () =>{
         }
     }
 
+    const handleRoot = (value) =>{
+        if (input === ''){
+            return input
+        } else{
+            return setInput(Math.sqrt(input))
+        }
+    }
+
+    const handleSquare = (value) =>{
+        if (input === ''){
+            return input
+        } else{
+            return setInput(input ** 2)
+        }
+    }
+
+    const handleCube = (value) =>{
+        if (input === ''){
+            return input
+        } else{
+            return setInput(input ** 3)
+        }
+    }
+
+    const handleBackspace = () =>{
+        setInput(input.substring(0, input.length - 1))
+    }
+
     return (
         <Container>
             <Wrapper>
@@ -36,25 +68,29 @@ const App = () =>{
                 <Button onClick={insertNumber}>7</Button>
                 <Button onClick={insertNumber}>8</Button>
                 <Button onClick={insertNumber}>9</Button>
-                <Button onClick={insertOperator}>/</Button>
+                <Button onClick={() => insertOperator('/')}><FaDivide/></Button>
+                <Button onClick={handleRoot}><FaSquareRootAlt/></Button>
             </Row>
             <Row>
                 <Button onClick={insertNumber}>4</Button>
                 <Button onClick={insertNumber}>5</Button>
                 <Button onClick={insertNumber}>6</Button>
-                <Button onClick={insertOperator}>*</Button>
+                <Button onClick={() => insertOperator('*')}><FaTimes/></Button>
+                <Button onClick={handleSquare}>x²</Button>
             </Row>
             <Row>
                 <Button onClick={insertNumber}>1</Button>
                 <Button onClick={insertNumber}>2</Button>
                 <Button onClick={insertNumber}>3</Button>
-                <Button onClick={insertOperator}>+</Button>
+                <Button onClick={() => insertOperator('+')}><FaPlus /></Button>
+                <Button onClick={handleCube}>x³</Button>
             </Row>
             <Row>
                 <Button onClick={insertOperator}>.</Button>
                 <Button onClick={insertNumber}>0</Button>
                 <Button onClick={() => setInput('')}>C</Button>
-                <Button onClick={insertOperator}>-</Button>
+                <Button onClick={() => insertOperator('-')}><FaMinus/></Button>
+                <Button onClick={handleBackspace}><TiBackspaceOutline/></Button>
             </Row>
             <Row>
                 <Button onClick={calculate}>=</Button>
--- src/utils/Operators/index.js
+++ src/utils/Operators/index.js
@@ -1,1 +1,1 @@
-export const arrOperators = ['*','/','+','-','.']
+export const arrOperators = ['*','/','+','-','.', "^"]
